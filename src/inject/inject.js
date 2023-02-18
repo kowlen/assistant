027ca6d419eda1db48262fd898b59f971216eec9
Kristina <yarevyakinakristina@gmail.com>
2023-02-18 11:56:59 +0700

    chat

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,12 +4,25 @@ chrome.extension.sendMessage({}, function(response) {
 		clearInterval(readyStateCheckInterval);
 
 		let assistant = `
-				<p>верстка</p>
+		<div class="chat" id="chat"></div>
+		<div class="question-wrap" onclick="openChat()">
+          <div class="question"> </div>
+         </div>
 		`;
 
 		document.querySelector('body').append(assistant);
 
 		console.log('Скрипт подключен')
+
+		function openChat () {
+			const chat = document.getElementById("chat");
+			
+			if (chat.style.display === "none") {
+				chat.style.display = "block";
+			} else {
+				chat.style.display = "none";
+			}
+		}
 		// var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
 		// recognition.lang = 'en-US';
 		// recognition.interimResults = false;
